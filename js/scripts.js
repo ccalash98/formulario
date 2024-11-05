79cc1fdba8d77ba3e61abce605e122ed872e2bb9
ccalash98 <cristtofher.ccalash@gmail.com>
2024-11-05 17:51:38 -0500

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+window.addEventListener("load", function() {
+    const loader = document.querySelector(".loader_bg");
+    if (loader) {
+        loader.style.display = "none"; // Oculta el loader cuando todo está cargado
+    }
+});
