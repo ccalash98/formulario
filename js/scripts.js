0427986cc92ce6fd16438fe0f121edfcf4a0321d
ccalash98 <cristtofher.ccalash@gmail.com>
2024-11-06 11:31:12 -0500

    se corrige y realiza adaptaciones

--- js/scripts.js
+++ js/scripts.js
@@ -2,20 +2,38 @@ document.addEventListener("DOMContentLoaded", function() {
     const titularSelect = document.getElementById("titular");
     const personaNaturalDiv = document.getElementById("personaNatural");
     const personaJuridicaDiv = document.getElementById("personaJuridica");
+    const serviciosDiv = document.getElementById("servicios");
+    const solicitudDiv = document.getElementById("solicitud");
+
+
 
     titularSelect.addEventListener("change", function() {
         const titularValue = titularSelect.value;
 
         // Mostrar u ocultar campos según la selección
         if (titularValue === "natural") {
-            personaNaturalDiv.style.display = "block";
+            personaNaturalDiv.style.display = "block";            
             personaJuridicaDiv.style.display = "none";
+            serviciosDiv.style.display = "block";
+            solicitudDiv.style.display = "block";
+            // Mostrar todos los elementos con animación
+            personaNaturalDiv.classList.add("show");
+            serviciosDiv.classList.add("show");
+            solicitudDiv.classList.add("show");
         } else if (titularValue === "juridica") {
             personaJuridicaDiv.style.display = "block";
             personaNaturalDiv.style.display = "none";
+            serviciosDiv.style.display = "block";
+            solicitudDiv.style.display = "block";
+            // Mostrar todos los elementos con animación
+            personaJuridicaDiv.classList.add("show");
+            serviciosDiv.classList.add("show");
+            solicitudDiv.classList.add("show");
         } else {
             personaNaturalDiv.style.display = "none";
             personaJuridicaDiv.style.display = "none";
+            serviciosDiv.style.display = "none";
+            solicitudDiv.style.display = "none";
         }
     });
 });
