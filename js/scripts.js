597c046940c641bffa0b225b73c395772f5bb859
ccalash98 <cristtofher.ccalash@gmail.com>
2024-11-05 19:38:24 -0500

    sinc

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,26 @@
-window.addEventListener("load", function() {
-    const loader = document.querySelector(".loader_bg");
-    if (loader) {
-        loader.style.display = "none"; // Oculta el loader cuando todo está cargado
-    }
+document.addEventListener("DOMContentLoaded", function() {
+    const titularSelect = document.getElementById("titular");
+    const personaNaturalDiv = document.getElementById("personaNatural");
+    const personaJuridicaDiv = document.getElementById("personaJuridica");
+
+    titularSelect.addEventListener("change", function() {
+        const titularValue = titularSelect.value;
+
+        // Mostrar u ocultar campos según la selección
+        if (titularValue === "natural") {
+            personaNaturalDiv.style.display = "block";
+            personaJuridicaDiv.style.display = "none";
+        } else if (titularValue === "juridica") {
+            personaJuridicaDiv.style.display = "block";
+            personaNaturalDiv.style.display = "none";
+        } else {
+            personaNaturalDiv.style.display = "none";
+            personaJuridicaDiv.style.display = "none";
+        }
+    });
 });
+function limitInputLength(element, maxLength) {
+    if (element.value.length > maxLength) {
+        element.value = element.value.slice(0, maxLength);
+    }
+}
